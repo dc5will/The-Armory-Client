e479d178f4aa146f0f1934042b9a7d3c42588e49
William Y Wong <wwong1987@gmail.com>
2019-04-23 11:39:08 -0700

    completed initial registration form

---
+++ src/RegistrationForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+export default function RegisterForm(props) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onRegister() {
+    try {
+      // insert API services for registration
+      props.history.replace("/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  return (
+    <main>
+      <div className="registrationForm">
+        <h2>Register</h2>
+        <form className='registration-form' onSubmit={e => e.preventDefault()}>
+          <div className="input-field">
+            <label htmlFor='registration-name-input'>Username{" "}</label>
+            <input
+              id='registration-name-input'
+              type="text"
+              placeholder="username"
+              value={username}
+              required
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor='registration-email-input'>Email{" "}</label>
+            <input
+              id='registration-email-input'
+              type="email"
+              placeholder="example@email.com"
+              value={email}
+              required
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor='registration-password-input'>Password{" "}</label>
+            <input
+              id="registration-password-input"
+              type="password"
+              placeholder="password"
+              value={password}
+              required
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="submit-button" onClick={onRegister}>
+            Register
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
