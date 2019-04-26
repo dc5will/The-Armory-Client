7d7298de327c8c741c94fdf2aea4abf45098bf64
William Y Wong <wwong1987@gmail.com>
2019-04-26 11:18:04 -0700

    removed some comments and very minor css

--- src/Components/GamesPage.js
+++ src/Components/GamesPage.js
@@ -7,41 +7,32 @@ export default function PartiesPage(props) {
   const [parties, setParties] = useState([]);
   const [gameInfo, setGame] = useState([]);
 
+  // Link from dashboard to Overwatch
   // http://localhost:3000/games/aa0e8ce9-1a71-42e7-804d-6838556fa6ed/parties
+  // Link from dashboard to FF
+  // http://localhost:3000/games/1c0aa6f7-0e03-4ceb-82de-ac53617f1b30/parties
 
   function getAllParties() {
-    return fetch(
-      `${config.API_ENDPOINT}${props.match.url}/parties`,
-      {
-        headers: {
-          authorization: `bearer ${TokenService.getAuthToken()}`
-        },
-        body: JSON.stringify(TokenService.getAuthToken)
-      }
-    ).then(res =>
+    return fetch(`${config.API_ENDPOINT}${props.match.url}/parties`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(TokenService.getAuthToken)
+    }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   }
-  
+
   function getGame() {
-    return fetch(
-      `${config.API_ENDPOINT}${props.match.url}`,
-      {
-        headers: {
-          authorization: `bearer ${TokenService.getAuthToken()}`
-        },
-        body: JSON.stringify(TokenService.getAuthToken)
-      }
-    ).then(res =>
+    return fetch(`${config.API_ENDPOINT}${props.match.url}`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(TokenService.getAuthToken)
+    }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
   }
-
-  // api/games/${gameId}
-  // http://localhost:8000/api/games/aa0e8ce9-1a71-42e7-804d-6838556fa6ed/ (overwatch)
-
-  // api/games/${gameId}/parties
-  // http://localhost:8000/api/games/aa0e8ce9-1a71-42e7-804d-6838556fa6ed/parties
 
   useEffect(() => {
     // populate party listing from API
@@ -51,18 +42,17 @@ export default function PartiesPage(props) {
         partyListing.push(party);
       });
       setParties(partyListing);
-    })
+    });
   }, []);
 
   useEffect(() => {
     // get game stuff
     getGame().then(games => {
       let gameInfo = {};
-      gameInfo = games
+      gameInfo = games;
       setGame(gameInfo);
-    })
+    });
   }, []);
-
 
   function generateSpots(party) {
     console.log(party);
@@ -71,10 +61,11 @@ export default function PartiesPage(props) {
         <div key={index}>
           <img
             src={
-              spot.filled
-                ? spot.filled.avatar_url
-                : "https://banner2.kisspng.com/20180503/lge/kisspng-batman-computer-icons-bane-avatar-5aebcc92207686.890462141525402770133.jpg"
+              (spot.filled)
+              ? spot.filled.avatar_url
+              : "https://banner2.kisspng.com/20180503/lge/kisspng-batman-computer-icons-bane-avatar-5aebcc92207686.890462141525402770133.jpg"
             }
+            className='avatar_url'
             alt="avatar_icon"
             width="60"
           />
@@ -106,14 +97,13 @@ export default function PartiesPage(props) {
     <main>
       <div className="container">
         <h2>{gameInfo.title}</h2>
-        <img src={gameInfo.image_url} alt='game-logo' width='200'/>
-        <h3>Active Parties</h3>
+        <h3>Current Active Parties</h3>
+        <img src={gameInfo.image_url} alt="game-logo" width="40" />
         <div className="parties-list">{generateParties()}</div>
         <button type="submit" className="join-party-button">
           +
         </button>
       </div>
-
       <CreatePartyForm />
     </main>
   );
