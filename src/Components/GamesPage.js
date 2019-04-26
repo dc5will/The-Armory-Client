04f5c448d3d2180b7352b06b4d2ae0f381f6e51f
William Y Wong <wwong1987@gmail.com>
2019-04-25 18:29:07 -0700

    consolidating games page, still working on it

--- src/Components/GamesPage.js
+++ src/Components/GamesPage.js
@@ -1,23 +1,52 @@
 import React, { useState, useEffect } from "react";
-import PartyApiService from '../services/parties-api-service';
-import CreatePartyForm from './CreatePartyForm';
+import CreatePartyForm from "./CreatePartyForm";
+import config from "../config";
+import TokenService from "../services/token-service";
 
 export default function PartiesPage(props) {
   const [parties, setParties] = useState([]);
-  // const [games, setGames] = useState('');
+  const [game, setGame] = useState("");
 
-  useEffect(
-    () => {
+  function getAllParties() {
+    // fetched specific parties id endpoint to test
+    return fetch(
+      `${config.API_ENDPOINT}/parties/fb1d3c63-6a72-4013-be82-5b523c1dd1cd/`,
+      {
+        headers: {
+          authorization: `bearer ${TokenService.getAuthToken()}`
+        },
+        body: JSON.stringify(TokenService.getAuthToken)
+      }
+    ).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  }
+
+  function getGameById(gameId) {
+    // grab all parties for a specific game
+    // fetched specific parties id endpoint to test
+    return fetch(`${config.API_ENDPOINT}/games/${gameId}/`, {
+      headers: {
+        authorization: `bearer ${TokenService.getAuthToken()}`
+      },
+      body: JSON.stringify(TokenService.getAuthToken)
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    );
+  }
+  // http://localhost:8000/api/games/aa0e8ce9-1a71-42e7-804d-6838556fa6ed/parties
+  // http://localhost:8000/api/games/aa0e8ce9-1a71-42e7-804d-6838556fa6ed/
+
+  useEffect(() => {
     // populate party listing from API
-      PartyApiService.getAllParties().then(parties => {
-        let partyListing = [];
-        parties.forEach(party => {
-          partyListing.push(party);
-        });
-        setParties(partyListing);
-      })
-    }
-  );
+    getAllParties().then(parties => {
+      let partyListing = [];
+      parties.forEach(party => {
+        partyListing.push(party);
+      });
+      setParties(partyListing);
+    });
+  });
 
   return (
     <main>
@@ -38,7 +67,9 @@ export default function PartiesPage(props) {
             </div>
           ))}
         </div>
-        <button type='submit' className='join-party-button'>Join</button>
+        <button type="submit" className="join-party-button">
+          Join
+        </button>
       </div>
 
       <CreatePartyForm />
