ab6cd022b23d601d9c7409ac44a8d745165db51d
Juicebox Jones <jderhalli@gmail.com>
2019-04-27 21:11:43 -0700

    started trying to implement socket, couldnt establish communication

--- src/Routes/PartyPage.js
+++ src/Routes/PartyPage.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useContext, useState } from "react";
-import TokenService from "../services/token-service";
-import config from "../config";
-import PartyContext from "../Contexts/partyContext";
-import GameContext from "../Contexts/gameContext";
+import React, { useEffect, useContext, useState } from 'react';
+import TokenService from '../services/token-service';
+import config from '../config';
+import PartyContext from '../Contexts/partyContext';
+import io from 'socket.io-client';
+let socket;
 
-export default function PartyPage() {
+export default function PartyPage(props) {
   const partyContext = useContext(PartyContext);
-  const gameContext = useContext(GameContext);
+  const [partyState, setParty] = useState('');
+  const party = partyState;
+
+  useEffect(() => {
+    getPartyById()
+      .then(res => setParty(res))
+      .then(partyContext.setParty(partyState));
+      socket = io('http://localhost:8000');
+  }, []);
+
 
   function getPartyById(props) {
     return fetch(
-      `${config.API_ENDPOINT}/parties/fb1d3c63-6a72-4013-be82-5b523c1dd1cd`,
+      `${config.API_ENDPOINT}/parties/fb1d3c63-6a72-4013-be82-5b523c1dd1cd`, //this will need to change back to props.match when the join party functionality is ready
       {
         headers: {
           authorization: `Bearer ${TokenService.getAuthToken()}`
@@ -19,22 +29,29 @@ export default function PartyPage() {
     ).then(res => (!res.ok ? TokenService.clearAuthToken() : res.json()));
   }
 
-  useEffect(() => {
-    getPartyById().then(res => partyContext.setParty(res));
-  }, []);
+  //probably no the right way to call socket.io, do I need useEffect?
+  //also, tried setting up prompt and triggering it during a state change
+  //but nothing was happening.
+  function leave(e, props){
+    e.preventDefault()
+      socket.emit('leave room', 'fb1d3c63-6a72-4013-be82-5b523c1dd1cd');
+    //console.log(window.history.back())
 
-  const party = partyContext.party;
-  console.log(party.reqs);
+  }
 
-  function generateReqs(reqs) {
-    console.log(reqs);
-    return reqs.map(req => {
-      return <li>{req.req_name}</li>;
+  function generateReqs(party) {
+    return party.reqs.map((req, i) => {
+      return <li key={i}>{req.req_name}</li>;
     });
   }
 
   function generateRoles(party) {
-    return party.roles.map(role => <li>{role.role_name}</li>);
+    return party.spots.map(spot => {
+      return spot.roles.map((role, i) => {
+        const user = spot.filled
+        return <li key={i}>{role.role_name} - {user!==null ? user.username: 'Available'}</li>;
+      });
+    });
   }
 
   function generateDisplayParty(party) {
@@ -42,10 +59,21 @@ export default function PartyPage() {
       <div>
         <h1>{party.title}</h1>
         <p>{party.description}</p>
+        <h3>Spots:</h3>
         <ul>{generateRoles(party)}</ul>
+        <h3>Requirements:</h3>
+        <ul>{generateReqs(party)}</ul>
       </div>
     );
   }
-  console.log(gameContext);
-  return <>{generateDisplayParty(party)}</>;
+
+
+  return (
+      <div>
+        <div>
+          {party ? generateDisplayParty(party) : 'Loading'}
+        </div>
+        <button onClick={e => leave(e)}>Leave party</button>
+      </div>
+  )
 }
