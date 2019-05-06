ab97f7a2edb044e44719399747237628332a98ee
William Y Wong <wwong1987@gmail.com>
2019-05-05 17:33:47 -0700

    reset password input fields on successful form submit

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,16 +1,16 @@
-import React, { useState, useContext, useEffect } from 'react';
-import UserContext from '../Contexts/userContext';
-import TokenService from '../services/token-service';
-import config from '../config';
-import AuthApiService from '../services/auth-api-service';
+import React, { useState, useContext, useEffect } from "react";
+import UserContext from "../Contexts/userContext";
+import TokenService from "../services/token-service";
+import config from "../config";
+import AuthApiService from "../services/auth-api-service";
 
 export default function UserProfile(props) {
-  const [email, setEmail] = useState('');
-  const [avatar_url, setAvatar_url] = useState('');
-  const [currentPass, setCurrentPass] = useState('');
-  const [newPass, setNewPass] = useState('');
-  const [confirmPass, setConfrimPass] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [avatar_url, setAvatar_url] = useState("");
+  const [currentPass, setCurrentPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const [error, setError] = useState("");
   const context = useContext(UserContext);
   const curr = context.user;
   const currentEmail = curr.email;
@@ -24,7 +24,7 @@ export default function UserProfile(props) {
   function populateContext() {
     const { user_id } = TokenService.parseAuthToken();
     return fetch(`${config.API_ENDPOINT}/user/${user_id}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
         authorization: `Bearer ${TokenService.getAuthToken()}`
       }
@@ -37,31 +37,40 @@ export default function UserProfile(props) {
     console.log(user);
     const { user_id } = TokenService.parseAuthToken();
     return fetch(`${config.API_ENDPOINT}/user/${user_id}`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
         authorization: `Bearer ${TokenService.getAuthToken()}`
       },
       body: JSON.stringify(user)
-    }).then(res => (!res.ok ? setError(res.error) : setError('Profile successfully updated')))
-    .then(props.update());
+    })
+      .then(res =>
+        !res.ok ? setError(res.error) : setError("Profile successfully updated")
+      )
+      .then(props.update());
   }
 
-  console.log(props.update)
+  // console.log(props.update)
 
   function confirmNewPassword(newPass, confirmPass) {
     if (newPass) {
       if (newPass === confirmPass) {
         let user = { email, avatar_url, password: newPass };
         saveChanges(user);
+        setNewPass("");
+        setConfirmPass("");
+        setCurrentPass("");
       } else {
-        setError('Passwords do not match');
-        return; }
+        setError("Passwords do not match");
+        return;
+      }
     } else {
       let user = { email, avatar_url };
-      saveChanges(user); }
+      saveChanges(user);
+      // setEmail('');
+    }
   }
-  
+
   function authorizeChanges(e) {
     e.preventDefault();
     console.log(currentEmail, currentPass);
@@ -73,7 +82,7 @@ export default function UserProfile(props) {
       .catch(error => setError(error.error));
   }
 
-  console.log(avatar_url)
+  console.log(avatar_url);
 
   return (
     <main>
@@ -84,12 +93,50 @@ export default function UserProfile(props) {
 
           <div className="input-field">
             <p>Choose a new avatar:</p>
-            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKh4dv2ykYZJuJ7vSszU4Vozhm1UtFmKQ_dwIXbQLjBUAb-hVU'} alt="ninja" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
-            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWdx9y2u5MR9pKaegRIT9WY3kbY6J_dB9qTxO2JIzHAkZCkYjQ'} alt="pikachu" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
-            <img src={'https://s3-us-west-2.amazonaws.com/files.geekgirlauthority.com/wp-content/uploads/2016/05/CuteSprayAvatars-76_OW_JP_400x400-300x300.png'} alt="soldier" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
-            <img src={'http://www.ffxivrealm.com/data/avatars/l/0/826.jpg?1371910121'} alt="ffcacti" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
-            <img src={'http://steamavatars.co/?media_dl=308'} alt="dota" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
-            <img src={'http://i66.tinypic.com/o9mqad.png'} alt="TJ" className="avatars" onClick={e => setAvatar_url(e.target.src)}/>
+            <img
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKh4dv2ykYZJuJ7vSszU4Vozhm1UtFmKQ_dwIXbQLjBUAb-hVU"
+              }
+              alt="ninja"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
+            <img
+              src={
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWdx9y2u5MR9pKaegRIT9WY3kbY6J_dB9qTxO2JIzHAkZCkYjQ"
+              }
+              alt="pikachu"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
+            <img
+              src={
+                "https://s3-us-west-2.amazonaws.com/files.geekgirlauthority.com/wp-content/uploads/2016/05/CuteSprayAvatars-76_OW_JP_400x400-300x300.png"
+              }
+              alt="soldier"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
+            <img
+              src={
+                "http://www.ffxivrealm.com/data/avatars/l/0/826.jpg?1371910121"
+              }
+              alt="ffcacti"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
+            <img
+              src={"http://steamavatars.co/?media_dl=308"}
+              alt="dota"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
+            <img
+              src={"http://i66.tinypic.com/o9mqad.png"}
+              alt="TJ"
+              className="avatars"
+              onClick={e => setAvatar_url(e.target.src)}
+            />
           </div>
 
           <div className="input-field">
@@ -116,14 +163,14 @@ export default function UserProfile(props) {
 
           <div className="input-field">
             <label htmlFor="profile-password-input">
-              Confirm New Password:{' '}
+              Confirm New Password:{" "}
             </label>
             <input
               id="profile-password-input"
               type="password"
               placeholder="change password"
               value={confirmPass}
-              onChange={e => setConfrimPass(e.target.value)}
+              onChange={e => setConfirmPass(e.target.value)}
             />
           </div>
 
