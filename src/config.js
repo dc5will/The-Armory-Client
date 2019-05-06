b8dec4a223bc437fc7e0f8434db28ccf96706eb6
William Y Wong <wwong1987@gmail.com>
2019-05-06 10:48:31 -0700

    added deployment scripts and now.json

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'super-secret-secret'
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://limitless-brushlands-45977.herokuapp.com/api',
+  TOKEN_KEY: process.env.REACT_APP_API_KEY
 }
