52e51075eb35cd8153ae5be29263b9fd61e11464
Juicebox Jones <jderhalli@gmail.com>
2019-05-08 08:28:35 -0700

    dashboard and 404 style

--- src/Routes/NotFound.js
+++ src/Routes/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import whichone from '../resources/whichone.gif'
+import './notFound.css'
 
 export default function PageNotFound(){
   return ( 
-    <div>
+    <div className='notFound-container'>
       <h1>404 Not Found</h1>
         <h3>What page were you looking for?</h3>
         <img className='NotFound' src={whichone} alt='Jared from Silicon Valley'/>
