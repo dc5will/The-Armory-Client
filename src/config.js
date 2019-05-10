7ecc96a88df19e913ce791072ea7fcea666f94f3
Juicebox Jones <jderhalli@gmail.com>
2019-05-10 09:08:53 -0700

    party img update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  // API_ENDPOINT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://limitless-brushlands-45977.herokuapp.com/api',
+   API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'https://limitless-brushlands-45977.herokuapp.com/api',
   IMAGES_ENDPOINT: 'http://localhost:8000/images',
   TOKEN_KEY: 'super-secret-secret'
 }
